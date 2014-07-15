bbcf39f8138b296521cdf90746fdc2b2ff158415
delbio <delbio87@gmail.com>
2014-07-16 00:04:05 +0200

    Update init_crontab.sh

--- init/init_crontab.sh
+++ init/init_crontab.sh
@@ -1,5 +1,21 @@
+min_args=1
+
+if (( $# < ${min_args} ))
+then
+    echo "Error config.cfg file is needed"
+    exit
+fi
+
+config_file=${1}
+
+if [ ! -f $recipient_file ];
+then
+        echo "Selected Config file: $config_file not found"
+        exit
+fi
+
 actual_dir=$(pwd)
-command="bash $(dirname ${actual_dir})/download_counter_per_day.sh $1 \"1 days ago\""
+command="cd $(dirname ${actual_dir}); bash download_counter_per_day.sh $config_file \"1 days ago\""
 job="10 00 * * * ${command}"
 echo "$job"
 #cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
